d3a97664171e41284cb5902b447aad71f04457a8
Destro <59611471+destro-code@users.noreply.github.com>
2025-08-06 14:09:40 +0100

    Update main.ts

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -11,39 +11,25 @@ interface TeacherInterface {
 }
 
 class Director implements DirectorInterface {
-  workFromHome(): string {
-    return "Working from home";
-  }
-  getCoffeeBreak(): string {
-    return "Getting a coffee break";
-  }
-  workDirectorTasks(): string {
-    return "Getting to director tasks";
-  }
+  workFromHome() { return 'Working from home'; }
+  getCoffeeBreak() { return 'Getting a coffee break'; }
+  workDirectorTasks() { return 'Getting to director tasks'; }
 }
 
 class Teacher implements TeacherInterface {
-  workFromHome(): string {
-    return "Cannot work from home";
-  }
-  getCoffeeBreak(): string {
-    return "Cannot have a break";
-  }
-  workTeacherTasks(): string {
-    return "Getting to work";
-  }
+  workFromHome() { return 'Cannot work from home'; }
+  getCoffeeBreak() { return 'Cannot have a break'; }
+  workTeacherTasks() { return 'Getting to work'; }
 }
 
 function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === "number" && salary < 500) {
+  if (typeof salary === 'number' && salary < 500) {
     return new Teacher();
   }
   return new Director();
 }
 
-console.log(createEmployee(200));
-console.log(createEmployee(1000));
-console.log(createEmployee("$500"));
+
 
 function isDirector(employee: Director | Teacher): employee is Director {
   return (employee as Director).workDirectorTasks !== undefined;
@@ -52,24 +38,20 @@ function isDirector(employee: Director | Teacher): employee is Director {
 function executeWork(employee: Director | Teacher): string {
   if (isDirector(employee)) {
     return employee.workDirectorTasks();
-  } else {
-    return employee.workTeacherTasks();
   }
+  return employee.workTeacherTasks();
 }
 
-console.log(executeWork(createEmployee(200)));
-console.log(executeWork(createEmployee(1000)));
+
 
 
 type Subjects = 'Math' | 'History';
 
 function teachClass(todayClass: Subjects): string {
-  if (todayClass === 'Math') {
-    return 'Teaching Math';
-  } else if (todayClass === 'History') {
-    return 'Teaching History';
-  }
+  if (todayClass === 'Math') return 'Teaching Math';
+  return 'Teaching History';
 }
 
-console.log(teachClass('Math'));
-console.log(teachClass('History'));
+
+
+
